8d741b30c59987e2068944bd4b0ed79bce7c8b79
sreehithreddy0202 <absreehithr@gmail.com>
2026-03-02 10:40:57 +0530

    UC8: Fix linked list - fast-slow pointers + reverse second half

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -12,7 +12,7 @@ public class PalindromeCheckerApp {
         System.out.print("Enter string: ");
         String original = sc.nextLine();
 
-        // Build linked list
+        // Build linked list (head insertion)
         ListNode head = null;
         for (char c : original.toCharArray()) {
             ListNode newNode = new ListNode(c);
@@ -20,22 +20,51 @@ public class PalindromeCheckerApp {
             head = newNode;
         }
 
-        // Check palindrome (simplified)
-        StringBuilder reversed = new StringBuilder();
-        ListNode temp = head;
-        while (temp != null) {
-            reversed.append(temp.data);
-            temp = temp.next;
-        }
-
-        if (original.equals(reversed.reverse().toString())) {
+        if (isPalindrome(head)) {
             System.out.println("Palindrome!");
         } else {
             System.out.println("Not a palindrome.");
         }
         sc.close();
     }
+
+    // UC8: Fast-slow pointer + reverse second half
+    static boolean isPalindrome(ListNode head) {
+        // Step 1: Find middle (fast-slow pointers)
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Step 2: Reverse second half
+        ListNode secondHalf = reverseList(slow);
+
+        // Step 3: Compare first half vs reversed second half
+        ListNode firstHalf = head;
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data) {
+                return false;
+            }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+        return true;
+    }
+
+    // Reverse linked list (second half)
+    static ListNode reverseList(ListNode head) {
+        ListNode prev = null, current = head;
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
 }
+
 
 
 
