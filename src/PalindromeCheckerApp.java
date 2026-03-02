5584c2bdaae76b4a8b9d400b0bb9bd02ea34dd7e
sreehithreddy0202 <absreehithr@gmail.com>
2026-03-02 10:08:35 +0530

    UC2: Hardcoded palindrome fixed

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,18 +1,12 @@
-import java.util.Scanner;
-
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter string: ");
-        String original = sc.nextLine();
+        String original = "racecar";  // ← THIS ONLY!
 
-        // Reverse using loop
         String reversed = "";
         for (int i = original.length() - 1; i >= 0; i--) {
             reversed += original.charAt(i);
         }
 
-        // Compare and display result
         if (original.equals(reversed)) {
             System.out.println("Palindrome!");
         } else {
